eec3b0b2e95657a43d0e14104934b0a49260ea47
metalogi <ed.niblett@gmail.com>
2017-12-16 17:15:14 -0800

    Moved all original files into folder

--- Assets/Remastered/Scripts/Utils/StraightLine.cs
+++ Assets/Remastered/Scripts/Utils/StraightLine.cs
@@ -7,6 +7,22 @@ public class StraightLine : AbstractLine {
     public Transform start;
     public Transform end;
 
+    public bool hidePoints = true;
+
+    public void Awake() {
+        if (hidePoints) {
+            Renderer r = start.GetComponent<Renderer>();
+            if (r != null) {
+                r.enabled = false;
+            }
+            r = end.GetComponent<Renderer>();
+            if (r != null) {
+                r.enabled = false;
+            }
+        }
+        
+    }
+
     public override Vector3 ReadPoint(float t) {
         t = Mathf.Clamp01(t);
         return Vector3.Lerp(start.position, end.position, t);
